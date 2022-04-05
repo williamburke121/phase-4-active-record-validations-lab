7c781273aca3179a3ed4ca49d3290614481cd926
William Warner Burke <williamburke121@gmail.com>
2022-04-05 15:30:26 -0400

    commit tree 8a08761f6ad1db0da3817484ad88d46eb4fd1046

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length:{minimum:250}
+    validates :summary, length:{maximum:250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]  }
+    validates :title, uniqueness: true
+
 end
